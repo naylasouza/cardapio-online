7eb69ed8000157af6212b56521464dc7dd2e07dc
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2025-09-05 15:51:32 -0300

    implementando logica de adicionar itens e atualizar carrinho de compra

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const cartCounter = document.getElementById("cart-count")
 const addressImput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = [];
 
 cartBtn.addEventListener("click", function(){
+    updateCartModal();
     carttModal.style.display = "flex"
 })
 
@@ -22,3 +24,59 @@ carttModal.addEventListener("click", function(event){
 closeModalBtn.addEventListener("click", function(){
     carttModal.style.display = "none"
 })
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+})
+
+function addToCart(name, price){
+   
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity += 1;
+        return;
+
+    }
+
+    cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+
+    updateCartModal()
+
+}
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div")
+
+        cartItemElement.innerHTML = `
+            <div> 
+            <div>
+            <p>${item.name}</p>
+            <p>${item.quantity}</p>
+            <p>${item.price}</p>
+            </div>
+            <div>
+            </div>
+                <button>
+                  Remover
+                </button>
+            </div>
+        `
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+}
